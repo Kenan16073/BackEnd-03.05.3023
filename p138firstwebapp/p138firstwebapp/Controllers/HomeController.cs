4b3df79f1d1eeb65dda13966948fae61bbda2a87
Kenan <kananmh@code.edu.az>
2023-05-03 22:20:46 +0400

    back

---
+++ p138firstwebapp/p138firstwebapp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace p138firstwebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public FileResult Index()
+        {
+
+        }
+
+
+        //public ViewResult Index()
+        //{
+        //    ViewResult viewResult = new ViewResult();
+        //    viewResult.ViewName= "test";
+        //    return viewResult;
+
+        //}
+
+
+
+        //public ContentResult Index()
+        //{
+        //    ContentResult result = new ContentResult();
+        //    result.Content = "p138 salam Content";
+        //    result.ContentType = "text";
+        //    result.StatusCode= 600;
+        //    return result;
+
+
+        //}
+        //public JsonResult Index()
+        //{
+        //    JsonResult jsonResult = new JsonResult("Salam");
+        //    jsonResult.StatusCode = 200;
+
+        //    return jsonResult;
+
+
+        //}
+    }
+}
